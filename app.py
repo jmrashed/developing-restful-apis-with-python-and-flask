320edf1e1cb40e59ccd1d98fb2fceb456a377573
Rashed Zaman <jmrashed@gmail.com>
2022-08-28 21:57:21 +0600

    update products.html page

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__ , template_folder='templates')
  
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = '123456'
 app.config['MYSQL_DB'] = 'sookh'
  
 mysql = MySQL(app)
